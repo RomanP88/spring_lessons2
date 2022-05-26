33f6a5fe234bfa1098b72e1eec025416e5c952f8
Roman <rasamaha1905@gmail.com>
2022-05-26 17:38:35 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/context/config/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.context.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+    @Configuration
+    @ComponentScan("ru.geekbrains.context")
+    public class ApplicationConfiguration {
+
+
+    }
+
+
